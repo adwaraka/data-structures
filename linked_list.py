f6f271a198ca123273d40c43c2cc7cdbafd03942
Arvind D <arvindd25@gmail.com>
2017-08-31 20:32:37 -0700

    added a search functionality

--- linked_list.py
+++ linked_list.py
@@ -1,3 +1,9 @@
+#             head
+#              |
+#              |
+#              V
+#  4, 9, 9, 8, 1
+
 class Node():
     def __init__(self, data=None, node=None):
         self.data = data
@@ -21,6 +27,15 @@ class SingleLinkedList():
         new_node.set_next(self.top)
         self.top = new_node #set the top to the new node added at the end
 
+    def search(self, data):
+        current = self.top
+        while current:
+            if current.get_data() == data:
+                print "Found"
+                break
+            else:
+                current = current.get_next()
+
     def print_list(self):
         current = self.top
         while current:
@@ -34,7 +49,9 @@ def main():
     list.insert("1")
     list.insert("9")
     list.print_list()
-
+    list.search("1")
+    list.search("-1")
+    list.search("9")
 
 if __name__ == "__main__":
     main()
