e791183f86447490dfc9ac5d5f3dd67180d7b549
Arvind D <arvindd25@gmail.com>
2019-03-24 15:07:18 -0700

    Adding hungry_rabbit challenge for advanced use of Data structures

---
+++ hungry_rabbit.py
@@ -0,0 +1,9 @@
+#Binary Maze Search
+# Driver program to test above function 
+if __name__ == "__main__": 
+    # Initialising the maze 
+    maze = [ [1, 0, 0, 0], 
+             [1, 1, 0, 1], 
+             [0, 1, 0, 0], 
+             [1, 1, 1, 1] ]          
+    findPath(maze) 
